0c97a2e79b17a3475fee637ce1648161f859f4bf
KJELD-DESKTOP\Kjeld <info@kjeld.me>
2017-01-01 20:06:20 +0100

    The old version was only usable with lights. This version is made to work with the scene system included in Home Assistant. This mains it contains no transision, brightness or color value's anymore.
    Also this mains it's usable with all the devices the user want to start on wakeup. Including sound, tv's, coffeepots etc.

--- README.md
+++ README.md
@@ -20,22 +20,14 @@ After this the clock alarm componenet can be initialized by inserting:
 
 ```
 clockalarm:
-	entity_id: light.personal
-	brightness: 255 
-	color: [255 200 115] 
-	color_temp: 370 
+	entity_id: scene.wakeup
+
 ```
 
-entity_id (required) should contain your light, or group of light.
+entity_id (required) should contain the scene you want to run on alarm time
 
-brightness (optional) the value the light will get after transition
-
-color: (optional) [red green blue] values for the color of the light
-
-color_temp: (optional) color temperature
 
 If no values are given for the optional fields it will take the default values written in the code snippet.
 
-Into your configuration file, Brightness, color and color_temp are optional.
 
 
--- clockalarm.py
+++ clockalarm.py
@@ -20,9 +20,6 @@ MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
 MIN_TIME_BETWEEN_FORCED_UPDATES = timedelta(seconds=60)
 
 CONF_ENTITY = 'entity_id'
-CONF_BRIGHTNESS = 'brightness'
-CONF_COLOR = 'color'
-CONF_COLOR_TEMP = 'color_temp'
 
 def setup(hass, config):
 	ENTITY_ID = config[DOMAIN].get(CONF_ENTITY)
@@ -33,43 +30,14 @@ def setup(hass, config):
 	else:
 		log.info('entity_id: ' + str(ENTITY_ID))
 
-	BRIGHTNESS = config[DOMAIN].get(CONF_BRIGHTNESS)
-	if BRIGHTNESS == None:
-		log.info('No brightness is given, default is used: 255')
-		BRIGHTNESS = 255
-	elif not isinstance(BRIGHTNESS, int):
-		log.error('brightness has the wrong type! It should be of type int')
-	else:
-		log.info('Brightness is set to: ' + str(BRIGHTNESS))
-
-	COLOR = config[DOMAIN].get(CONF_COLOR)
-	if COLOR == None:
-		log.info('No color is given, default value is used: [255, 200, 115]')
-		COLOR = [255, 200, 115]
-	elif not isinstance(COLOR, list):
-		log.error('color has the wrong type! It should be of type list')
-	else:
-		log.info('Color is set to: ' + str(COLOR))
-	
-	COLOR_TEMP = config[DOMAIN].get(CONF_COLOR_TEMP)
-	if COLOR_TEMP == None:
-		log.info('No color temp is given, default value is used: 370')
-		COLOR_TEMP = 370
-	elif not isinstance(COLOR_TEMP, int):
-		log.error('color_temp has the wrong type! It should be of type int')
-	else:
-		log.info('Color temp is set to: ' + str(COLOR_TEMP))
-	
 	@Throttle(MIN_TIME_BETWEEN_UPDATES, MIN_TIME_BETWEEN_FORCED_UPDATES)
 	def update(call):
 		wake_up_time = hass.states.get('input_select.wake_up_time').state
-		transition_time = hass.states.get('input_select.transition_time').state
-		transition_time = int(transition_time) * 60
-		time_to_execute = (datetime.datetime.now() + datetime.timedelta(seconds=int(transition_time))).time().isoformat()[0:5]
+		time_to_execute = (datetime.datetime.now() + datetime.timedelta(seconds=int(0))).time().isoformat()[0:5]
 		if hass.states.get('input_boolean.' + WEEKDAY[datetime.datetime.today().weekday()]).state == 'off':
 			log.info('No alarm for today')
 		elif wake_up_time == time_to_execute:
-			hass.services.call('homeassistant', 'turn_on', {"entity_id": ENTITY_ID, "transition":transition_time, "brightness":BRIGHTNESS, "rgb_color":COLOR, "color_temp":COLOR_TEMP })
+			hass.services.call('homeassistant', 'turn_on', {"entity_id": ENTITY_ID })
 
 		return True
 	hass.bus.listen(homeassistant.core.EVENT_TIME_CHANGED, update)
